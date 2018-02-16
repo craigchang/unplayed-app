c184ff26e97f90b530888b921a61f32357752f9e
Craig Chang <craigchang@gmail.com>
2018-02-15 23:36:34 -0800

    refactor unplayed list vue component

--- src/components/UnplayedList.vue
+++ src/components/UnplayedList.vue
@@ -4,7 +4,7 @@
     <p>{{listDescription}}</p>
     <ul class="list-group">
       <!-- <UnplayedListItem v-for="(item, key) in unplayedList" :item="item" :key="item.id"></UnplayedListItem> -->
-      <UnplayedListItem class="list-group-item list-group-item-action flex-column align-items-start" v-for="(item, key) in unplayedList" :item="item" :key="key"></UnplayedListItem>
+      <UnplayedListItem class="list-group-item list-group-item-action flex-column align-items-start" v-for="(item, key) in unplayedList" :item="item" :item-key="key"></UnplayedListItem>
     </ul>
   </div>
 </template>
@@ -13,7 +13,8 @@
 /* eslint-disable */
 import showdown from 'showdown'
 import $ from 'jquery'
-import UnplayedListItem from './UnplayedListItem'
+import UnplayedListItem from '@/components/UnplayedListItem'
+import UnplayedListParser from '@/helpers/unplayed-list-parser'
 
 export default {
   name: 'UnplayedList',
@@ -23,7 +24,6 @@ export default {
   props: ['listTitle', 'fileName'],
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
       listDescription: '',
       unplayedList: []
     }
@@ -37,57 +37,19 @@ export default {
         return response.text();
       })
       .then(text => {
-        let converter = new showdown.Converter();
-        let html = converter.makeHtml(text);
-        html = html.replace(/\(/g, '<span>').replace(/\)/g, '</span>');
-        let parsedHtmlArray = $.parseHTML(html);
+        let parsedHtmlArray = UnplayedListParser.convertMarkdownToHtmlDom(text);
         for(var i = 0; i < parsedHtmlArray.length; i++) {
           if (parsedHtmlArray[i].tagName === 'P') {
             this.listDescription = parsedHtmlArray[i].outerText;
           }
-
           if (parsedHtmlArray[i].tagName === 'UL') {
-            this.unplayedList = this.getListArray(parsedHtmlArray[i]);
+            this.unplayedList = UnplayedListParser.parseListUl(parsedHtmlArray[i]);
           }
         }
+
       }).catch(e => {
-        console.log(e)
+        console.log(e);
       })
-  },
-  methods: {
-    getListArray: (listUl) => {
-      let gamesLiElements = Array.from(listUl.getElementsByTagName('li'));
-      let listObj = [];
-
-      gamesLiElements.forEach((element) => {        
-        let link = '';
-        if (element.getElementsByTagName('a').length > 0)
-          link = element.getElementsByTagName('a')[0].getAttribute('href');
-
-        let gameTitle = '';
-        if (element.getElementsByTagName('a').length > 0)
-          gameTitle = element.getElementsByTagName('a')[0].innerText;
-        else
-          gameTitle = element.childNodes[0].textContent.trim(); // For now assume game title is always the first line
-
-        let consoleName = '';
-        if (element.getElementsByTagName('span').length > 0)
-          consoleName = element.getElementsByTagName('span')[0].innerText;
-
-        let comment = '';
-        if (element.getElementsByTagName('span').length > 1)
-          comment = element.getElementsByTagName('span')[1].innerText;
-
-        listObj.push({
-          link,
-          gameTitle,
-          consoleName,
-          comment
-        });
-      });
-
-      return listObj
-    }
   }
 }
 </script>
---
+++ src/helpers/unplayed-list-parser.js
@@ -0,0 +1,43 @@
+import showdown from 'showdown'
+import $ from 'jquery'
+
+export default {
+  convertMarkdownToHtmlDom: (text) => {
+    let html = (new showdown.Converter()).makeHtml(text);
+    html = html.replace(/\(/g, '<span>').replace(/\)/g, '</span>');
+    return $.parseHTML(html);
+  },
+  parseListUl: (listUl) => {
+    let gamesLiElements = Array.from(listUl.getElementsByTagName('li'));
+    let listObj = [];
+
+    gamesLiElements.forEach((element) => {        
+      let link = '';
+      if (element.getElementsByTagName('a').length > 0)
+        link = element.getElementsByTagName('a')[0].getAttribute('href');
+
+      let gameTitle = '';
+      if (element.getElementsByTagName('a').length > 0)
+        gameTitle = element.getElementsByTagName('a')[0].innerText;
+      else
+        gameTitle = element.childNodes[0].textContent.trim(); // For now assume game title is always the first line
+
+      let consoleName = '';
+      if (element.getElementsByTagName('span').length > 0)
+        consoleName = element.getElementsByTagName('span')[0].innerText;
+
+      let comment = '';
+      if (element.getElementsByTagName('span').length > 1)
+        comment = element.getElementsByTagName('span')[1].innerText;
+
+      listObj.push({
+        link,
+        gameTitle,
+        consoleName,
+        comment
+      });
+    });
+
+    return listObj;
+  }  
+}
